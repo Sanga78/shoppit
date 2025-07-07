5464aa3c80532e4a33be79ecf71229adf5faeb51
sanga78 <kelvinkipkosgaeisanga@gmail.com>
2025-07-07 15:55:58 +0100

    Update NavBar.jsx
    created a navbar

--- shoppit_app/src/components/ui/NavBar.jsx
+++ shoppit_app/src/components/ui/NavBar.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-      
+      <nav className={'navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3'}></nav>
+      <div className="container">
+        <Link className="navbar-brand fw-bold text-uppercase" to="/">SHOPPIT</Link>
+      </div>
     </div>
   )
 }
